17fcc405a41aa899d9e593ca5fe4bc28e3172b04
Hanjiseok1 <heemoon25@naver.com>
2022-07-12 20:53:12 +0900

    int commit

---
+++ src/main/java/com/springcoot/studyjiseok/StudyJiseokApplication.java
@@ -0,0 +1,13 @@
+package com.springcoot.studyjiseok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJiseokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJiseokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcoot/studyjiseok/StudyJiseokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcoot.studyjiseok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJiseokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
